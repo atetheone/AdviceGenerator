f7dddc4b6454d06f90dfc3aa634ae527009ccb24
atetheone <atevirran@gmail.com>
2022-05-18 22:51:25 +0100

    get data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements {
+export class AppComponent implements OnInit {
   title = 'AdviceGenerator';
-  
+  adviceApiUrl = "https://api.adviceslip.com/advice";
+  slip!: { id: number, advice: string};
+
+
+  constructor(
+    private http: HttpClient
+  )
+  async ngOnInit() {
+    this.http.get<any>(this.adviceApiUrl)
+      .subscribe(
+        advice => this.slip = advice.slip,
+        error => console.log(`Error: ${error}`)
+      )
+  }
 }
