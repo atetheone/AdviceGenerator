7fa852380472ffc319222af2a120eef9a88d1d91
atetheone <atevirran@gmail.com>
2022-05-19 08:19:47 +0100

    device component updated

--- src/app/components/advice/advice.component.ts
+++ src/app/components/advice/advice.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-advice',
   templateUrl: './advice.component.html',
   styleUrls: ['./advice.component.sass']
 })
 export class AdviceComponent implements OnInit {
-
   adviceApiUrl = "https://api.adviceslip.com/advice";
   slip!: { id: number, advice: string};
 
-
+  hiddenLoader = false;
   constructor(
     private http: HttpClient
   ) {
@@ -19,14 +17,24 @@ export class AdviceComponent implements OnInit {
   }
 
   async ngOnInit(): Promise<any> {
+    this.generateAdvice();
+  }
+
+  async generateAdvice() {
+    this.hiddenLoader = false;
     await this.http.get<any>(this.adviceApiUrl)
     .subscribe(
       advice => {
+        if (advice) {
+          this.hiddenLoader = true;
+        }
         console.log(JSON.stringify(advice, null, 3))
         this.slip = advice.slip;
       },
       error => console.log(`Error: ${error}`)
     )
   }
+
+
 
 }
